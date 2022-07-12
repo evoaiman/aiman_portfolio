d388d19702178d8ceeeb63c56e84097a1060e8a5
evoaiman <67497191+evoaiman@users.noreply.github.com>
2022-07-12 18:47:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo 'hey bro..';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- head definitions go here -->
+    </head>
+    <body>
+        <!-- the content goes here -->
+      hey
+    </body>
+</html>
